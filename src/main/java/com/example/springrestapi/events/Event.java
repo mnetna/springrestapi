7f65142eb6eb2500445b61c3d3ddd7bc61e2f619
HF195 <sssolee90@gmail.com>
2019-08-07 21:51:43 +0900

    Initial commit

---
+++ src/main/java/com/example/springrestapi/events/Event.java
@@ -0,0 +1,26 @@
+package com.example.springrestapi.events;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+//@Data 애노테이션은 상호참조 여지때문에 stackoverflow가 발생할 여지가 있음
+@Builder @AllArgsConstructor @NoArgsConstructor
+@Getter @Setter @EqualsAndHashCode(of = "id")
+public class Event {
+
+    private Integer id;
+    private String name;
+    private String description;
+    private LocalDateTime beginEnrollmentDateTime;
+    private LocalDateTime closeEnrollmentDateTime;
+    private LocalDateTime beginEventDateTime;
+    private LocalDateTime endEventDateTime;
+    private String location; // (optional) 이게 없으면 온라인 모임
+    private int basePrice; // (optional)
+    private int maxPrice; // (optional)
+    private int limitOfEnrollment;
+    private boolean offline;
+    private boolean free;
+    private EventStatus eventStatus;
+}
